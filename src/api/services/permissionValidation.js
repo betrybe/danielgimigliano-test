f20efdbf6fda912b78910c4d0ed542075af30feb
Gimigliano <daniel.gimigliano@protonmail.com>
2021-11-27 20:53:09 -0300

    adding enum

--- src/api/services/permissionValidation.js
+++ src/api/services/permissionValidation.js
@@ -2,6 +2,7 @@ const { tokenValidation } = require('./tokenValidation');
 const models = require('../models');
 
 const { MALFORMED_TOKEN_JWT } = require('../helpers');
+const { roles } = require('../enums');
 
 const permissionValidation = async (idRecipe, authorization) => {
     const { userId } = await models.recipesModel.getRecipeById(idRecipe);
@@ -10,7 +11,7 @@ const permissionValidation = async (idRecipe, authorization) => {
     if (err) return { status, err };
     
     const { _id, role } = data;
-    if (userId === _id || role === 'admin') return { data: data };
+    if (userId === _id || role === roles.user.admin) return { data };
 
     return MALFORMED_TOKEN_JWT;
 };
