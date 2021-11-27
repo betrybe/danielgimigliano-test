96c9c04f4140d8fb167b92b09ac0ac5dbc38d286
Gimigliano <daniel.gimigliano@protonmail.com>
2021-11-27 20:53:36 -0300

    adding integration testing

---
+++ src/integration-tests/recipe/recipe.test.js
@@ -0,0 +1,20 @@
+// const app = require("../../api/app");
+
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+
+// chai.should();
+// chai.use(chaiHttp);
+
+// describe('Listagem de receita', () => {
+//     it('Crie um endpoint para a listagem de receitas', () => {
+//         chai.request(app)
+//             .get('/recipes')
+//             .end((err, res) => {
+//                 chai.assert.isNull(err);
+//                 chai.assert.isNotEmpty(res);
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+// });
